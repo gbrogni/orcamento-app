b6f44c87fb61ca2423c39c6271203db9950a3d64
gbrogni <harden13brogni@gmail.com>
2023-01-06 10:26:45 -0300

    Criado ts para vendas e custos mês a mês

--- src/app/components/orcamento/orcamento.component.ts
+++ src/app/components/orcamento/orcamento.component.ts
@@ -1,11 +1,11 @@
 import { MatSelectModule } from '@angular/material/select';
 import { HeaderService } from './../template/header/header.service';
-import {  Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { OrcamentoService } from './orcamento.service';
 import AnoConsult, { Orcamento } from './orcamento.model';
 import { ActivatedRoute } from '@angular/router';
 
-export interface ano{
+export interface ano {
   anoValue: string;
   anoViewValue: string;
 }
@@ -18,109 +18,135 @@ export interface ano{
 })
 export class OrcamentoComponent implements OnInit {
   orcamentos: Orcamento[] = [{
-    codEmpresa:0,
-    tipo:'string',
-    data:new Date(),
+    codEmpresa: 0,
+    tipo: 'string',
+    data: new Date(),
     codProduto: 0,
-    nomeProduto:'string',
-    qtde:0,
+    nomeProduto: 'string',
+    qtde: 0,
     custoUni: 0,
-    valorVenda:0,
-    custoTotal:0,
-   }]
+    valorVenda: 0,
+    custoTotal: 0,
+  }]
 
-  displayedColumns: string[] = ['codProduto', 'nomeProduto', 'custoUni', 'valorVenda', 'custoTotal'];
+  displayedColumns: string[] = ['codProduto', 'nomeProduto', 'custoUni', 'valorVenda', 'custoTotal', 'VendaJaneiro'];
   dataSource: Orcamento[] = [];
 
   Anos: ano[] = [
-    {anoValue: '2019', anoViewValue: '2019'},
-    {anoValue: '2020', anoViewValue: '2020'},
+    { anoValue: '2019', anoViewValue: '2019' },
+    { anoValue: '2020', anoViewValue: '2020' },
   ]
 
   constructor(
-    public orcamentoService: OrcamentoService, 
+    public orcamentoService: OrcamentoService,
     private headerService: HeaderService,
-    @Inject(MatSelectModule)public data: AnoConsult,
-    @Inject(MatSelectModule)public mes: Orcamento,
+    @Inject(MatSelectModule) public data: AnoConsult,
+    @Inject(MatSelectModule) public mes: Orcamento,
     public route: ActivatedRoute
-    ) 
-    { 
+  ) {
     headerService.headerData = {
       title: 'Orçamento',
       icon: 'home',
-      routeUrl:''
+      routeUrl: ''
     }
   }
 
 
-   
- 
+
+
   hidden!: boolean;
   hiddenn!: boolean;
 
 
   ngOnInit(): void {
     this.hidden = false;
-    console.log(this.meses);
   }
 
-  ocultaVenda(){
+  ocultaVenda() {
     this.hidden = !this.hidden;
-}
+  }
 
-  ocultaCusto(){
-  this.hiddenn = !this.hiddenn;
-}
+  ocultaCusto() {
+    this.hiddenn = !this.hiddenn;
+  }
 
-  consultar(){
-    this.orcamentoService.getProdutos(this.data.AnoConsult).subscribe(orcamentosdados=>{
+  consultar() {
+    this.orcamentoService.getProdutos(this.data.AnoConsult).subscribe(orcamentosdados => {
       this.orcamentos = orcamentosdados;
     })
   }
 
-  calcular(){
-    this.orcamentoService.getByYear(this.data.AnoConsult).subscribe(orcamentosdados=>{
+  transformaMes(mesEmNumero: number) {
+    return {
+      0: 'Janeiro',
+      1: 'Fevereiro',
+      2: 'Março',
+      3: 'Abril',
+      4: 'Maio',
+      5: 'Junho',
+      6: 'Julho',
+      7: 'Agosto',
+      8: 'Setembro',
+      9: 'Outubro',
+      10: 'Novembro',
+      11: 'Dezembro'
+    }[mesEmNumero]
+  }
+
+  calcular() {
+    this.orcamentoService.getByYear(this.data.AnoConsult).subscribe(orcamentosdados => {
       let orcamentomap = this.agrupaProd(orcamentosdados);
-      for (const [codProduto, produtos] of orcamentomap){
+      let orcamento = [];
+      for (const [codProduto, produtos] of orcamentomap) {
         const mesMap = this.agrupaProdMes(produtos)
-        console.log(mesMap)
-        const [prod] = produtos;
-        for (const [mes, prodMeses] of mesMap){
-          const custoTotal = prod.custoUni * prodMeses.lenght
-          // const valortotal = 
+        let [prod] = produtos;
+        let produtoMes : any = {};
+        ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'].map((mes: string) => {
+          produtoMes[mes] = {
+            custoTotal: 0, 
+            valorVenda: 0
+          }
+        })
+        for (const [data, prodMeses] of mesMap) {
+          const custoTotal = prodMeses.reduce((prev: number, next: any) => prev + next.custoTotal, 0)
+          const valorVenda = prodMeses.reduce((prev: number, next: any) => prev + next.valorVenda, 0)
+          const mes = this.transformaMes(data)
+          produtoMes = {
+            ...produtoMes,
+            [mes as string]: {
+              custoTotal,
+              valorVenda
+            }
+          }
         }
+        prod = { ...prod, ...produtoMes }
+        orcamento.push(prod)
       }
-      this.orcamentos=orcamentosdados;
+      console.log(orcamento)
+      console.log(orcamentomap)
+      this.orcamentos = orcamento;
     })
   }
 
-  agrupaProd(produtos: any[]){
+  agrupaProd(produtos: any[]) {
     const mapProdutos = new Map()
-    for (const produto of produtos){
+    for (const produto of produtos) {
       const mapValores = mapProdutos.get(produto.codProduto) || []
       mapValores.push(produto)
       mapProdutos.set(produto.codProduto, mapValores)
     }
-    return mapProdutos; 
+    return mapProdutos;
   }
 
-  agrupaProdMes(produtos: any[]){
+  agrupaProdMes(produtos: any[]) {
     const mapMeses = new Map()
-    for (const produto of produtos){
-      const mes = new Date(produto.mes).getMonth()
-      const mapValores = mapMeses.get(mes) || []
+    for (const produto of produtos) {
+      const data = new Date(produto.data).getMonth()
+      const mapValores = mapMeses.get(data) || []
       mapValores.push(produto)
-      mapMeses.set(mes, mapValores)
+      mapMeses.set(data, mapValores)
     }
     return mapMeses;
   }
-
-  
-  criaArray(): any[]{
-    return new Array(this.orcamentoService.getByYear(this.mes.data?.getMonth()));
-
-}
-
-meses:any[] = this.criaArray();
 
 }
